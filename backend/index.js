e26199a6cd32e6f75e06c6b888782d083f3f554d
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-06-18 19:06:27 +0530

    db setup

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import connectDB from './db/connectDB.js';
+
+const app = express();
+app.use(cors({
+    Credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(helmet({
+    crossOriginResourcePolicy: false,
+}));
+
+const PORT=8080 || process.env.PORT;
+
+
+app.get("/",(req, res) => {
+    res.status(200).json({
+        message: "Welcome to Evergo API"
+    });
+})
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to database:", err);
+    process.exit(1);
+  });
---
+++ backend/models/user.model.js
@@ -0,0 +1,99 @@
+import mongoose, { mongo } from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+
+   name:{
+         type: String,
+         required: [true, 'Name is required'],
+         
+   },
+   email : {
+            type: String,
+            required: [true, 'please enter a Email address'],
+            unique: true,
+            match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+   },
+   password: {
+            type: String,
+            required: [true, 'Please enter a password'],
+   },
+   avatar : {
+      type: String,
+      default:""
+   },
+   mobile: {
+      type: Number,
+      required: [true, 'Please enter a mobile number'],
+      match: [/^\d{10}$/, 'Please enter a valid mobile number'],
+      default: null
+   },
+
+   refreshToken: {
+      type: String,
+      default: ""
+   },
+ 
+   verify_email:{
+        type: Boolean,
+        default: false
+   },
+
+   last_login_date:{
+        type: Date,
+        default: ""
+   },
+
+   status : {
+     type: String,
+     enum : ['active', 'inactive', 'banned'],
+     default: 'active'
+   },
+   address_details: [
+         {
+            type: mongoose.Schema.ObjectId,
+            ref: 'address'
+         }
+   ],
+   shopping_details: [
+         {
+            type: mongoose.Schema.ObjectId,
+            ref: 'cartProduct'
+         }
+   ],
+   orderHistory: [
+         {
+            type: mongoose.Schema.ObjectId,
+            ref: 'order'
+         }
+   ],
+   forgot_password_otp : {
+    type:String,
+    default: null
+   },
+   forgot_password_expiry: {
+      type: Date,
+      default: ""
+   },
+   role : {
+    type: String,
+    enum: ['ADMIN', 'USER'],
+    default: 'USER'
+   },
+
+
+
+
+
+
+
+
+},{
+    timestamps: true,
+});
+
+
+const UserModel=mongoose.model("User",userSchema);
+
+
+export default UserModel;
+
