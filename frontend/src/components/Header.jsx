7f386861c3fec5771e262d100549ea4cc725295f
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-06-21 09:22:38 +0530

    frontend commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import logo from '../assets/Evegro.png'
+
+
+
+const Header = () => {
+  return (
+    <header className='h-20 shadow-md sticky top-0'>
+           <div className='container mx-auto '>
+            {/* Logo  */}
+             <div>
+                 <div>
+                    <img src={logo} alt="Evegro Logo" className='h-20 w-20' />
+
+
+                 </div>
+                
+                
+                </div> 
+            
+            {/* Search */}
+
+
+
+            {/* login and my cart */}
+
+
+
+
+            </div>
+    </header>
+  )
+}
+
+export default Header
