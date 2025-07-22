377e7ccfe176a280698ae08e9ef04106b45a3bc0
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-07-22 12:06:24 +0530

    upload-ui

--- frontend/src/components/UploadCategoryModel.jsx
+++ frontend/src/components/UploadCategoryModel.jsx
@@ -6,12 +6,68 @@ const UploadCategoryModel = ({close}) => {
         name: "",
         image: ""
     });
+
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;   
+        setData((prevData) =>{
+            return {
+                ...prevData,
+                [name]: value
+            }
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
+
+
   return (
     <section className='fixed top-0 bottom-0 left-0 right-0 p-4  bg-neutral-800/60 flex items-center justify-center'>
                <div className='bg-white max-w-4xl w-full p-4 rounded '>
-                     <button className='w-fit blovl'>
+                <div className='flex items-center justify-between '>
+                    <h1 className='font-semibold '>Category</h1>
+                     <button onClick={close} className='w-fit block ml-auto'>
                                   <IoClose size={25}/>
                      </button>
+
+
+                </div>
+                  <form className='my-3 grid gap-2 ' onSubmit={handleSubmit}>
+                       <div className='grid gap-1 '>
+                          <label id='category-name'>Name</label>
+                          <input
+                          type='text'
+                          id='category-name'
+                          placeholder='Enter category name'
+                          value={data.name
+                          }
+                          name='name'
+                          onChange={handleOnChange}
+                          className='bg-blue-50 p-2 border border-blue-100 focus-within:border-amber-400 outline-none rounded'
+                          />
+                       </div>
+
+                       <div className='grid gap-1'>
+                        <p>Image</p>
+                           <div className='flex  gap-4 flex-col lg:flex-row  items-center '>
+                             <div className='border bg-blue-50 h-36 w-full  lg:w-36 flex items-center justify-center rounded '>
+                                 <p className='text-sm text-neutral-300'>No Image</p>
+                            </div>
+                            <label htmlFor="uploadCategoryImage">
+                                  <div disabled={!data.name} className={`border ${!data.name ? "bg-grey-400":"bg-amber-400" }
+                            px-4 py-2 rounded cursor-pointer
+                            `}>
+                                Upload Image
+                            </div>
+                            <input type='file'  id='uploadCategoryImage' className='hidden'/>
+                            </label>
+                           
+                           </div>
+                       </div>
+                  </form>
                </div>
     </section>
   )
