83af38409e8c0d5391804ca5f2d68f8b526ac8aa
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-09-27 20:36:39 +0530

    product category by id display

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'
 const ProductListPage = () => {
+  const params=useParams();
+  
+
+  console.log("params", params);
   return (
-    <div>
-        ProductListPage
-        
-        
-        
-        </div>
+  //  <section className='sticky top-24 lg:top-20'> 
+  //  <div className='container sticky top-24 mx-auto grid grid-cols-[90px,1fr] md:grid-cols-[200px,1fr] lg:grid-cols-[280px,1fr] '> 
+  //   {/* sidebar sub category */}
+  //    <div className='bg-red-500 min-h-[79vh]'> 
+      
+  //      </div> {/* product list */} 
+  //      <div className='bg-green-600'> 
+  //       product 
+  //       </div>
+  //        </div>
+  //         </section>
+
+  <section className="flex border border-black min-h-[79vh]">
+  {/* Sidebar */}
+  <div className=" w-[90px] md:w-[200px] lg:w-[280px]">
+     
+  </div>
+
+  {/* Product list */}
+  <div className=" flex-1">
+    product
+  </div>
+</section>
+
+
+
+
   )
 }
 
